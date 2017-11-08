3c70f0f5a2c1699c3fc2a41ee54d27f8ace01780
André Neto da Silva <andre.netodasilva@he-arc.ch>
2017-11-08 14:45:47 +0100

    Project created

---
+++ TP1/GanderNetoDaSilva.py
@@ -0,0 +1,14 @@
+def selection():
+    pass
+
+def mutate():
+    pass
+
+def evaluate():
+    pass
+
+def ga_solve(file=None, gui=True, maxtime=0):
+    pass
+
+if __name__ == '__main__':
+    ga_solve()
