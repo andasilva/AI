6787a13ef80ee01edc8c36b5fd7939186e0b59d9
root <angelkiro.android@gmail.com>
2017-11-30 00:31:06 +0100

    First version with random population generation, simple crossing and simple mutation

--- TP1/GanderNetoDaSilva.py
+++ TP1/GanderNetoDaSilva.py
@@ -1,22 +1,117 @@
 import pygame
 from pygame.locals import KEYDOWN, QUIT, MOUSEBUTTONDOWN, K_RETURN, K_ESCAPE
+from math import hypot, sqrt
 import sys
+import random
+import argparse
+from copy import deepcopy
 
 mutation_rate = 0.1
 cross_over_rate = 0.6
 population_size = 20
+number_of_survivors = 10
 
-#Contient la population
-population = []
+# Contient la population
+populations = []
 
-#Villes récupéré depuis le fichier/gui
+# Villes récupéré depuis le fichier/gui
 list_cities = []
 
-#Résultat à dessiner
+# Best fitness reached
+best_chromosome = None
+
+# Résultat à dessiner
 cities_to_draw = []
 
-def populate(size):
-    pass
+# has all the distance between two cities
+distances = []
+
+# Survivors after selections
+survivors = []
+
+# Pygame attributs
+font = None
+screen = None
+city_color = [10, 10, 200]  # blue
+city_radius = 3
+font_color = [255, 255, 255]  # white
+
+
+def draw_path(city_path):
+    screen.fill(0)
+    pygame.draw.lines(screen, city_color, True, city_path)
+    pygame.display.flip()
+
+
+def launch_gui(file):
+    """
+    Init Pygame GUI
+    """
+    global screen, font
+
+    screen_x = 500
+    screen_y = 500
+
+    pygame.init()
+    pygame.display.set_mode((screen_x, screen_y))
+    pygame.display.set_caption('Le voyageur de commerce')
+    screen = pygame.display.get_surface()
+    font = pygame.font.Font(None, 30)
+
+    # Collecting Data by file or click
+    if file is None:
+        collecting = True
+    else:
+        collecting = False
+        open_file(file)
+
+    def draw(positions):
+        """
+        Draw the connections between cities position
+        """
+        global screen, font
+        screen.fill(0)
+        for pos in positions:
+            pygame.draw.circle(screen, city_color, pos, city_radius)
+        text = font.render("Nombre: %i" % len(positions), True, font_color)
+        text_rect = text.get_rect()
+        screen.blit(text, text_rect)
+        pygame.display.flip()
+
+    while collecting:
+        counter = 0
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit(0)
+            elif event.type == KEYDOWN and event.key == K_RETURN:
+                collecting = False
+            elif event.type == MOUSEBUTTONDOWN:
+                cities_to_draw.append(pygame.mouse.get_pos())
+                list_cities.append(City("v{}".format(counter), pygame.mouse.get_pos()))
+                draw(cities_to_draw)
+                counter += 1
+
+    # Calculate all distance
+    calculate_all_distance()
+
+    # Populate after collecting or reading the file
+    populate(population_size)
+
+    # Find the "best" path
+    processing()
+
+    draw_path(cities_to_draw)
+
+    text = font.render("Un chemin, pas le meilleur!", True, font_color)
+    text_rect = text.get_rect()
+    screen.blit(text, text_rect)
+    pygame.display.flip()
+
+    while True:
+        event = pygame.event.wait()
+        if event.type == KEYDOWN:
+            break
+
 
 def open_file(path):
     if path:
@@ -28,79 +123,116 @@ def open_file(path):
         print("File doesn't exist")
 
 
-def launch_gui():
-    screen_x = 500
-    screen_y = 500
-
-    city_color = [10, 10, 200]  # blue
-    city_radius = 3
-
-    font_color = [255, 255, 255]  # white
-
-    pygame.init()
-    pygame.display.set_mode((screen_x, screen_y))
-    pygame.display.set_caption('Le voyageur de commerce')
-    screen = pygame.display.get_surface()
-    font = pygame.font.Font(None, 30)
-
-    def draw(positions):
-        screen.fill(0)
-        for pos in positions:
-            pygame.draw.circle(screen, city_color, pos, city_radius)
-        text = font.render("Nombre: %i" % len(positions), True, font_color)
-        textRect = text.get_rect()
-        screen.blit(text, textRect)
-        pygame.display.flip()
+def populate(size):
+    """ Create an initial population """
+    for i in range(size):
+        populations.append(Chromosome(random.sample(list_cities, len(list_cities))))
 
 
+def calculate_all_distance():
+    for i in range(len(list_cities)):
+        distances.append([])
+        for j in range(len(list_cities)):
+            if i == j:
+                distances[i].append(0)
+            else:
+                city1 = list_cities[i]
+                city2 = list_cities[j % len(list_cities)]
+                x1 = city1.pos[0]
+                x2 = city2.pos[0]
+                y1 = city1.pos[1]
+                y2 = city2.pos[1]
+                distance = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+                distances[i].append(distance)
 
-    collecting = True
 
-    while collecting:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                sys.exit(0)
-            elif event.type == KEYDOWN and event.key == K_RETURN:
-                collecting = False
-            elif event.type == MOUSEBUTTONDOWN:
-                cities_to_draw.append(pygame.mouse.get_pos())
-                list_cities.append(City("bidon",pygame.mouse.get_pos()))
-                draw(cities_to_draw)
-
-    screen.fill(0)
-    pygame.draw.lines(screen, city_color, True, cities_to_draw)
-    text = font.render("Un chemin, pas le meilleur!", True, font_color)
-    textRect = text.get_rect()
-    screen.blit(text, textRect)
-    pygame.display.flip()
-
+def processing():
     while True:
-        event = pygame.event.wait()
-        if event.type == KEYDOWN:
-            break
+        evaluate()
+        selection()
+        crossing()
+        mutate()
+
+
+def evaluate():
+    """ Evaluate the population """
+    for i in range(len(populations)):
+        chromosome = populations[i]
+        total_distance = 0
+        cities = chromosome.list_cities
+
+        for i in range(len(cities)):
+            city1 = cities[i]
+            city2 = cities[(i + 1) % len(cities)]
+            index_city1 = next(i for i, item in enumerate(list_cities) if item.name == city1.name)
+            index_city2 = next(i for i, item in enumerate(list_cities) if item.name == city2.name)
+            total_distance += distances[index_city1][index_city2]
+
+        chromosome.fitness = total_distance
 
 
 def selection():
     """Sélectionner une sous-partie de la population qui servira de base à la population suivante"""
-    pass
+    global cities_to_draw, best_chromosome, survivors
+
+    populations.sort(key=lambda x: x.fitness)
+
+    if best_chromosome is None:
+        best_chromosome = deepcopy(populations[0])
+        cities_to_draw = [city.pos for city in best_chromosome.list_cities]
+        draw_path(cities_to_draw)
+    else:
+        if populations[0].fitness < best_chromosome.fitness:
+            best_chromosome = deepcopy(populations[0])
+            cities_to_draw = [city.pos for city in best_chromosome.list_cities]
+            draw_path(cities_to_draw)
+
+    survivors = populations[:number_of_survivors]
+
 
 def crossing():
-    pass
+    global populations
+    cutting_point = int(len(list_cities) * cross_over_rate)
+    populations[:] = []
+    survivors.append(best_chromosome)
+
+    # Feel populations with new chromosomes
+    for i in range(population_size):
+        choice = random.sample(range(len(survivors) - 1), 2)
+        first_slice_1 = survivors[choice[0]].list_cities[:cutting_point]
+        second_slice_1 = survivors[choice[1]].list_cities[cutting_point:]
+
+        first_slice_2 = survivors[choice[0]].list_cities[cutting_point:]
+        second_slice_2 = survivors[choice[1]].list_cities[:cutting_point]
+
+        chromosome_prototype = first_slice_1 + second_slice_1
+        chromosome_prototype_tool = first_slice_2 + second_slice_2  # used to get the others repeated values to exchange
+
+        duplicates_value = [x for n, x in enumerate(chromosome_prototype) if x in chromosome_prototype[:n]]
+        duplicates_value_to_exchange = [x for n, x in enumerate(chromosome_prototype_tool) if
+                                        x in chromosome_prototype_tool[:n]]
+        # Swap values
+        for index, city in enumerate(chromosome_prototype):
+            if city in duplicates_value:
+                chromosome_prototype[index] = duplicates_value_to_exchange[0]
+                del duplicates_value[duplicates_value.index(city)]
+                del duplicates_value_to_exchange[0]
+
+        populations.append(Chromosome(chromosome_prototype))
 
 
 def mutate():
-    print('mutate')
-
-
-def evaluate():
-    print("evaluate")
+    for chromosome in populations:
+        for index in range(len(list_cities)):
+            if index != 0:
+                if round(random.uniform(0, 1), 2) <= mutation_rate / 2:
+                    random_position = random.randint(0, len(list_cities) - 1)  # todo random_position != index
+                    chromosome.list_cities[index], chromosome.list_cities[random_position] = \
+                        chromosome.list_cities[random_position], chromosome.list_cities[index]
 
 
 def ga_solve(file=None, gui=True, maxtime=0):
-    if gui:
-        launch_gui()
-    else:
-        open_file(sys.argv[1])
+    launch_gui(file)
 
 
 class City(object):
@@ -109,7 +241,15 @@ class City(object):
         self.pos = pos
 
 
-if __name__ == '__main__':
-    open_file("Ressources/data/pb005.txt")
-    ga_solve()
+class Chromosome(object):
+    def __init__(self, cities_path):
+        self.list_cities = cities_path
+        self.fitness = 0
 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Gander & Neto da Silva André AI Algorithm")
+    # parser.add_argument('--nogui')
+
+    ga_solve(sys.argv[1])
+    # ga_solve()
